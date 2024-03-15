ce65466092b087f1663cb80d063f0f74b6ef2546
alumnoDAW <cosquillo00@gmail.com>
2024-03-15 13:55:38 +0100

    vuelto a crear inicio

--- src/git_eclipse/Gestion_Versiones.java
+++ src/git_eclipse/Gestion_Versiones.java
@@ -6,6 +6,9 @@ public class Gestion_Versiones {
 		// TODO Auto-generated method stub
 
 		System.out.println("Menu de opciones");
+		System.out.println("Hola mundo");
+		
+		int variable = 0;
 	}
 
 }
